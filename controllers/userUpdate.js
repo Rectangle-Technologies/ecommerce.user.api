c3018a3ae2120be5052f8e3745cf916973dc7214
Samyak Shah <samyakshah123@gmail.com>
2022-08-08 15:33:54 +0530

    User update API created

---
+++ controllers/userUpdate.js
@@ -0,0 +1,26 @@
+const User = require("../models/user");
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const { email, firstName, lastName, contact, address } = req.body;
+    user.email = email;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.contact = contact;
+    user.address = address;
+    await user.save();
+    res.status(200).json({
+      message: "User updated successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong",
+    });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const userFetchController = require("../controllers/userFetch");
+const userUpdateController = require("../controllers/userUpdate");
 const authMiddleware = require("../middleware/auth");
-
+``;
 // URL: /user/:id
 // METHOD: GET
 router.get("/:id", authMiddleware, userFetchController.getUserById);
@@ -9,5 +10,32 @@ router.get("/:id", authMiddleware, userFetchController.getUserById);
 // URL: /user/all
 // METHOD: GET
 router.get("/all", authMiddleware, userFetchController.getAllUsers);
+
+// URL: /user/update
+// METHOD: PUT
+router.put(
+  "/update",
+  authMiddleware,
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("firstName")
+      .not()
+      .isEmpty()
+      .withMessage("Please enter your first name"),
+    body("lastName").not().isEmpty().withMessage("Please enter your last name"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    body("contact").custom((value, { req }) => {
+      var regex = /^[6-9]\d{9}$/;
+      if (!regex.test(value)) {
+        throw new Error("Please enter a valid contact number");
+      }
+      return true;
+    }),
+    body("address").not().isEmpty().withMessage("Please enter your address"),
+  ],
+  userUpdateController.updateUser
+);
 
 module.exports = router;
