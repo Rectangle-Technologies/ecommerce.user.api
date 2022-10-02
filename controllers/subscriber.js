62b5f526ea3573f9bc81d1767bc48868de04c7b4
Samyak Shah <samyakshah123@gmail.com>
2022-10-02 13:02:07 +0530

    Check for existing subscriber

--- controllers/subscriber.js
+++ controllers/subscriber.js
@@ -11,8 +11,12 @@ exports.addSubscriber = async (req, res) => {
                 .json({ message: "Validation error", errors: errors.array() });
         }
         const { email } = req.body
+        const subs = await Subscriber.findOne({ email })
+        if (subs) {
+            return res.status(200).json({ message: 'You are already subscribed!' })
+        }
         const subscriber = await Subscriber.create({ email })
-        res.status(200).json({ message: 'Subscriber added', subscriber })
+        res.status(200).json({ message: 'Thank you for subscribing!', subscriber })
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: err.message || 'Something went wrong' })
